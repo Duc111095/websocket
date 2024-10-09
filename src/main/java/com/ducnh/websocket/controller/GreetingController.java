5f43249010c03fc031a9575b1a5478ca2d7d4c19
Duc111095 <huynhduc111095@gmail.com>
2024-10-10 00:02:14 +0700

    add UI

--- src/main/java/com/ducnh/websocket/controller/GreetingController.java
+++ src/main/java/com/ducnh/websocket/controller/GreetingController.java
@@ -11,6 +11,7 @@ public class GreetingController {
     @MessageMapping("/hello")
     @SendTo("/topic/greetings")
     public Greeting greeting(HelloMessage message) throws Exception {
+        System.out.println(message);
         Thread.sleep(1000);
         return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
     }
